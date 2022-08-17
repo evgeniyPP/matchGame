a81d3a5c1062f5e3c2be2dc71a1683c2adb9a62c
Eugene Petryankin <epetryankin@devvela.com>
2022-08-18 02:02:18 +0300

    v2

--- script.js
+++ script.js
@@ -1,52 +1,106 @@
-let spots = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+const OPENED_CLASSNAME = 'card_opened';
+const SOLVED_ATTRIBUTE = 'data-solved';
+const EMOJIS = ['🐳', '🐲', '🌞', '🌸', '🍑', '🍆', '🏀', '💩'];
 
-for (let j = 0; j < 2; j++){
-  let numbers = [1,2,3,4,5,6,7,8];
-  for (let i = 0; i < 8; i++){
-    let randomSpot = Math.floor(Math.random() * spots.length);
-    let spot = spots[randomSpot];
+const cards = [...document.querySelectorAll('.card')];
 
-    let randomNum = Math.floor(Math.random() * numbers.length);
-    let number = numbers[randomNum];
+const cardsIdsInRandomOrder = randomizeArray(cards.map(card => getCardId(card)));
 
-    let spotElement = document.getElementById(spot.toString());
-    spotElement.innerHTML = number;
+cardsIdsInRandomOrder.forEach((id, index) => {
+  const card = cards.find(c => getCardId(c) === id);
 
-    spots.splice(randomSpot, 1);
-    numbers.splice(randomNum, 1);
+  if (index >= EMOJIS.length) {
+    index -= EMOJIS.length;
   }
+
+  card.querySelector('.card__face').textContent = EMOJIS[index];
+});
+
+/**
+ * @type {Element}
+ */
+let selectedCard;
+let selectedCardId = '';
+let selectedCardEmoji = '';
+let isSecondCard = false;
+let movesCount = 0;
+let foundCardsCount = 0;
+
+cards.forEach(card =>
+  card.addEventListener('click', () => {
+    if (isSecondCard) return;
+    if (card.hasAttribute(SOLVED_ATTRIBUTE)) return;
+
+    const cardId = getCardId(card);
+    const cardEmoji = card.querySelector('.card__face').textContent;
+
+    if (selectedCardId === cardId) return;
+
+    if (!selectedCardId) {
+      selectedCard = card;
+      selectedCardId = cardId;
+      selectedCardEmoji = cardEmoji;
+    } else {
+      isSecondCard = true;
+    }
+
+    card.classList.add(OPENED_CLASSNAME);
+
+    if (!isSecondCard) return;
+
+    if (selectedCardEmoji === cardEmoji) {
+      selectedCard.setAttribute(SOLVED_ATTRIBUTE, 'true');
+      card.setAttribute(SOLVED_ATTRIBUTE, 'true');
+      foundCardsCount += 2;
+      selectedCard = null;
+      selectedCardId = '';
+      selectedCardEmoji = '';
+      movesCount++;
+
+      setTimeout(() => {
+        isSecondCard = false;
+      }, 800);
+
+      if (foundCardsCount === cards.length) {
+        setTimeout(() => {
+          alert(`Congradulations! You won in ${movesCount} moves!`);
+        }, 1000);
+      }
+      return;
+    }
+
+    setTimeout(() => {
+      selectedCard.classList.remove(OPENED_CLASSNAME);
+      card.classList.remove(OPENED_CLASSNAME);
+    }, 1000);
+    setTimeout(() => {
+      selectedCard = null;
+      selectedCardId = '';
+      isSecondCard = false;
+      movesCount++;
+    }, 1500);
+  })
+);
+
+/**
+ * @param {Element} card
+ */
+function getCardId(card) {
+  return card.getAttribute('data-id');
 }
 
-let tempOpen = [];
+/**
+ * @template T
+ * @param {T[]} array
+ * @returns {T[]}
+ */
+function randomizeArray(array) {
+  const output = [...array];
 
-const show = id => {
-  let element = document.getElementById(id);
-  element.style.visibility = 'visible';
-  tempOpen.push(id);
-};
-
-const hide = id => {
-  let element = document.getElementById(id);
-  element.style.visibility = 'hidden';
-};
-
-const check = id => {
-  let element = document.getElementById(id);
-  element.style.visibility === 'visible' ? undefined : show(id);
-  let timer = setTimeout(timeOut, 1250);
-};
-
-const timeOut = () => {
-  if (tempOpen.length >= 2) {
-    if (tempOpen.length === 2 && document.getElementById(tempOpen[0]).innerHTML === document.getElementById(tempOpen[1]).innerHTML) {
-      document.getElementById(tempOpen[0]).removeEventListener("click", check);
-      document.getElementById(tempOpen[1]).removeEventListener("click", check);
-      tempOpen = [];
-
-    }
-    tempOpen.forEach(spot => {
-      hide(spot);
-    });
-    tempOpen = [];
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [output[i], output[j]] = [output[j], output[i]];
   }
-};
+
+  return output;
+}
